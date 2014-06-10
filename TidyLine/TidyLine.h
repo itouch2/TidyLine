a6137b02306755576ca18e0ea452dba239b1b76f
Tu You <Chandler@TumatoMacBook-Air.local>
2014-06-10 22:44:06 +0800

    First commit

---
+++ TidyLine/TidyLine.h
@@ -0,0 +1,13 @@
+//
+//  TidyLine.h
+//  TidyLine
+//
+//  Created by Tu You on 14-6-10.
+//  Copyright (c) 2014年 Tu You. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface TidyLine : NSObject
+
+@end
